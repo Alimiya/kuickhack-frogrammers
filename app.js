bae8b3e15351ed923f57c72ab93b2114a6b201bf
Alimiya <211523@astanait.edu.kz>
2024-04-30 17:10:32 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('html', require('ejs').renderFile)
+
+app.set('view engine', 'html')
+app.set('layout', 'layouts/layout')
+
+app.use('/views', express.static(path.join(__dirname + '/views')))
+app.use('/public', express.static(path.join(__dirname + '/public')))
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(expressLayouts)
+
+app.use((req, res, next) => {
+    res.status(404).render('errors/error')
+})
+
+
+const start = async () => {
+    try {
+        await mongoose
+            .connect(process.env.MONGODB_URI)
+            .then(() => {
+                console.log("Database is connected")
+            })
+            .catch((err) => {
+                console.error('Internal server error')
+            })
+        app.listen(process.env.PORT, () => {
+            console.log(`http://localhost:${process.env.PORT}`)
+        })
+    } catch (err) {
+        console.error('Internal server error')
+    }
+}
+
+start()
+
+module.exports = app
