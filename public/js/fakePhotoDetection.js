bfc531a3a75ad0ff79477223b75e8b7a33a2ccd3
jadeshonan <khegay00@gmail.com>
2024-05-01 18:36:12 +0500

    fake photo detection

--- public/js/fakePhotoDetection.js
+++ public/js/fakePhotoDetection.js
@@ -1,26 +1,26 @@
 async function loadModels(){
-            await faceapi.nets.ssdMobilenetv1.loadFromUri('/public/jsmodels/faceapi');
-            await faceapi.nets.faceLandmark68Net.loadFromUri('/public/jsmodels/faceapi');
-            await faceapi.nets.faceRecognitionNet.loadFromUri('/public/jsmodels/faceapi');
+        await faceapi.nets.ssdMobilenetv1.loadFromUri('/public/jsmodels/faceapi');
+        await faceapi.nets.faceLandmark68Net.loadFromUri('/public/jsmodels/faceapi');
+        await faceapi.nets.faceRecognitionNet.loadFromUri('/public/jsmodels/faceapi');
         }
-        async function detectFaces(input) {
-                const img = await faceapi.fetchImage(input);
-                const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
-                return detections;
-            }
-        async function compareFaces(img1, img2) {
-                const face1 = await detectFaces(img1);
-                const face2 = await detectFaces(img2);
-                const faceMatcher = new faceapi.FaceMatcher(face1);
-                const bestMatch = faceMatcher.findBestMatch(face2.descriptor);
-                console.log(bestMatch.toString());
-            }
+async function detectFaces(input) {
+        const img = await faceapi.fetchImage(input);
+        const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
+        return detections;
+    }
+async function compareFaces(img1, img2) {
+        const face1 = await detectFaces(img1);
+        const face2 = await detectFaces(img2);
+        const faceMatcher = new faceapi.FaceMatcher(face1);
+        const bestMatch = faceMatcher.findBestMatch(face2.descriptor);
+        console.log(bestMatch.toString());
+    }
    
-        async function main() {
-                await loadModels();
-                const img1 = getImgInput('img');
-                const img2 = getImgInput('doc');
-                await compareFaces(img1, img2);
-            }
+async function main() {
+        await loadModels();
+        const img1 = getImgInput('img');
+        const img2 = getImgInput('doc');
+        await compareFaces(img1, img2);
+    }
 
-        main();
+main();
